5ff86905c7261c18b76a5a6c7a6e5096f90c9cc7
jiejen <jiezhang0123@gmail.com>
2025-02-09 23:19:02 -0600

    lint

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,8 @@ import 'package:flutter/material.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MyApp());
 }
