a0ba1a867730820067ccb8c9f5c87cc63bfe5f04
jiejen <jiezhang0123@gmail.com>
2025-03-13 01:29:45 -0500

    forgot pw

---
+++ lib/screens/forgot_password_page.dart
@@ -0,0 +1,116 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+class ForgotPasswordPage extends StatefulWidget {
+  const ForgotPasswordPage({super.key});
+
+  @override
+  State<ForgotPasswordPage> createState() => _ForgotPasswordPageState();
+}
+
+class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
+  final _emailController = TextEditingController();
+  bool _isButtonEnabled = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _emailController.addListener(_checkFields);
+  }
+
+  void _checkFields() {
+    setState(() {
+      _isButtonEnabled = _emailController.text.isNotEmpty &&
+          _emailController.text.contains('@');
+    });
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    super.dispose();
+  }
+
+  Future _onResetPassword() async {
+    try {
+      await FirebaseAuth.instance
+          .sendPasswordResetEmail(email: _emailController.text.trim());
+      showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              content: Text('Password link sent! Check your email.'),
+            );
+          });
+    } on FirebaseAuthException catch (e) {
+      print(e);
+      showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              content: Text(e.message.toString()),
+            );
+          });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 32),
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                      'Enter your email and we will send you a password reset link',
+                      style: Theme.of(context).textTheme.bodyMedium)),
+              const SizedBox(height: 20),
+              TextField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  labelText: 'Enter your email',
+                  labelStyle: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                        color: Theme.of(context).hintColor,
+                      ),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(6),
+                      borderSide: BorderSide(color: Color(0xFFE8EDEC))),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(6),
+                    borderSide: BorderSide(
+                        color:
+                            Color(0xFFE8EDEC)), // Border color when not focused
+                  ),
+                  floatingLabelBehavior: FloatingLabelBehavior.never,
+                ),
+              ),
+              const SizedBox(height: 20),
+              SizedBox(
+                width: double.infinity,
+                height: 44,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    disabledBackgroundColor: Theme.of(context)
+                        .colorScheme
+                        .primary
+                        .withAlpha((255 * 0.5).toInt()), // Background Color
+                    disabledForegroundColor: Colors.white70, //Text Color
+                  ),
+                  onPressed: _isButtonEnabled
+                      ? () {
+                          _onResetPassword();
+                        }
+                      : null,
+                  child: const Text('Reset password'),
+                ),
+              ),
+            ]),
+          ),
+        ));
+  }
+}
